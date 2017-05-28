e0f2b95f6516b649cf099798c0a7ff337d243aaf
kupshah <kupshah@users.noreply.github.com>
2017-05-28 17:39:01 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+import json
+import sys
+#import networkx
+#import requests
+#from bottle import route, run, post, request, app
+import random
+from urllib.parse import urlencode
+from urllib.request import Requests, urlopen
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def webhook():
+    data = request.get_json()
+    if data['name'] != 'flappy-bot':
+        msg = "This is a new test message, {} \n This is a reply to '{}'".format(data['name'],data["text"])
+        
+        send_message(msg)
+
+        return "ok",200
+
+def send_message(msg):
+    url = "https://api.groupme.com/v3/bots/post"
+    bot_id = '288f9e18900355d4b59ec6f717'
+    data = {"bot_id" : bot_id, "text" : msg}
+    data = json.dumps(data)
+    r = requests.post('https://api.groupme.com/v3/bots/post', message)
+    #request = Request(url,urlencode(data).encode())
+    #json = urlopen(request).read().decode()
+        
+
+
+
+
+
+
+
+
+
+
+    
